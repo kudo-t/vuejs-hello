c1f5d7dff827e4daed5f576587e62fe7d7b21e2d
kudo-t <46235312+kudo-t@users.noreply.github.com>
2020-05-05 03:32:39 +0900

    Update:msg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="That's one small step for mankind, one giant leap for me."/>
   </div>
 </template>
 
